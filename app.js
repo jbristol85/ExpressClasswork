0de1e16ef556571a75ad3f94bb759e56f474272e
Josh Bristol <josh.bristol2@gmail.com>
2018-01-21 16:14:41 +0000

    Level 1 Classwork

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response){
+	response.send('Hello World');
+});
+
+app.get('/name', function(request, response){
+	var name = "Josh Bristol";
+	response.json(name);
+});
+
+app.get('/redirect', function(request, response){
+	response.redirect(301, '/surprise');
+});
+
+app.get('/date', function(request, response){
+	var date = new Date();
+	response.send(date);
+});
+
+app.listen(process.env.PORT, function(){
+	console.log('Listening on port '+ process.env.PORT);
+	console.log('Local IP address is ' + process.env.IP);
+});
